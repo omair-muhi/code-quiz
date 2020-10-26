407a51b69aba717c235805ad8e2e08eebabba19b
Omair Muhi <omairmuhi@icloud.com>
2020-10-26 09:01:59 -0400

    Subject: First version of application Problem: NA Solution: NA Note: Tested to ensure all questions can be cycled through to end

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// GLOBAL DATA
 // Static array of objects containing quiz questions
 var quizQuestion = [{
         questionString: "What is HTML an acronym for?",
@@ -21,7 +22,9 @@ var quizQuestion = [{
         choices: ["Square braces", "Curly braces", "Round braces"]
     }
 ]
-
+var currentQuestion = 0;
+// GLOBAL DATA
+// BUTTONS
 function createButton(buttonString) {
     var btn = document.createElement("BUTTON");
     btn.innerHTML = buttonString;
@@ -32,23 +35,63 @@ function createButton(buttonString) {
 function deleteButton(buttonObject) {
     document.getElementById("dynamic-content").removeChild(buttonObject);
 }
-
-function handleStartButton(event) {
-    alert("Starting the quiz now...");
-    deleteButton(event.target);
+// BUTTONS
+// RENDERING
+function renderInitialPlaceholders() {
+    // 1. Create heading element
+    var h3Tag = document.createElement("H3");
+    h3Tag.setAttribute("id", "quiz-question");
+    document.getElementById("dynamic-content").appendChild(h3Tag);
+    // 2. Create ordered list of choices 
+    var olTag = document.createElement("OL");
+    olTag.setAttribute("id", "quiz-choices");
+    document.getElementById("dynamic-content").appendChild(olTag);
+    // 3. Create three LIs with one button each as a child
+    for (var i = 0; i < quizQuestion[0].choices.length; i++) {
+        // create LI element to hold button
+        var liTag = document.createElement("LI");
+        liTag.setAttribute("id", ("choice-" + i));
+        document.getElementById("quiz-choices").appendChild(liTag);
+        // create button with choice string
+        var choiceButton = document.createElement("BUTTON");
+        choiceButton.setAttribute("id", ("choice-button-" + i));
+        document.getElementById("choice-" + i).appendChild(choiceButton);
+    }
+    // add a handler for buttons
+    olTag.addEventListener("click", handleChoiceButtons);
 }
 
 function renderQuestion(questionObject) {
-
+    // update question string in <H> element
+    document.getElementById("quiz-question").innerText = questionObject.questionString;
+    // update button text
+    for (var i = 0; i < questionObject.choices.length; i++) {
+        document.getElementById("choice-button-" + i).innerHTML = questionObject.choices[i];
+    }
+}
+// RENDERING
+// HANDLERS
+function handleChoiceButtons(event) {
+    // alert("A choice was clicked!");
+    // Display next question
+    if (currentQuestion < 5)
+        renderQuestion(quizQuestion[currentQuestion++]);
 }
 
+function handleStartButton(event) {
+    // alert("Starting the quiz now...");
+    deleteButton(event.target);
+    // Create placeholders for rendering questions
+    renderInitialPlaceholders();
+    // Display first question
+    renderQuestion(quizQuestion[currentQuestion++]);
+}
+// HANDLERS
+// INIT
 function initApplication() {
-
     // Initialize welcome screen
     var startQuizButton = createButton("START QUIZ");
     startQuizButton.addEventListener("click", handleStartButton);
-    // Display first question
-
 }
-
+// MAIN
 initApplication();
